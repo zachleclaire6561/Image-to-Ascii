fc644adab8cf14511712f588987f5a8df044b8a8
Zach LeClaire <zacharyjleclaire@gmail.com>
2021-03-27 01:12:34 -0700

    Many features

--- test.sh
+++ test.sh
@@ -1,37 +1,110 @@
-get_dimensions() {
-    image=$1
-    lower=$((${#image}+1))
-    upper=$((${#image}+2))
-    echo $lower $upper
-    result=`magick identify ${image}`
-    #result is in form of [file name] [file type] [dimensions]. 
-    #File type is max 4 char & dimensions max 4-4, totalling 13 chars 
-    result=${result:lower:upper}
-    echo $result
+#returns character for a specific chunk
+get_character(){
+    arr=("$@")
+    #echo "${arr[@]}"
+
+    length=${#arr[@]}
+    if [[ $length > 1 ]]; then
+        x_len=${arr[length-1]}
+        y_len=$(($length/$x_len))
+        echo $x_len $y_len
+        
+    else
+        echo "An error occured. get_character did not recieve a valid array"
+    fi
 }
+
+#generates characer from avgerages of chunk
+get_char(){
+    avg_r=$1
+    avg_b=$2
+    avg_g=$3
+
+    if [[ -z $4 ]]; then
+        background="light"
+    else
+        background="dark"
+    fi
+
+    char_list=("@" "%" "#" "&" "=" "+" "-" ":" "," ".")
+
+    incr_size=${$((3*255/${#char_list}))%%.*}
+    
+    #choose characters solely based on total screen area they occupy
+    #IDEA: try non-linear scaling for magnitude
+    magnitude=$avg_r+$avg_b+$avg_g
+
+    if [[ $background -eq "dark" ]]; then
+        # background is dark, so we emphasize higher magnitudes, or closer to light - (255,255,255)
+       dist=$((3*255-$magnitude))
+    else
+        # background is light, so we emphasize lower magnitudes, or closer to dark - (0,0,0)
+        dist=$(($magnitude))
+    fi
+
+    index=${$((magnitude%incr_size))%%*.}
+    echo ${char_list[@]}
+}
+
+#itterate through 
 
 print_image(){
-    #convert image to array
-    x_dim=$1
-    y_dim=$2
-    x_dim_print=$3
-    y_dim_print=$4
+    #convert image to array - mistake bc bash can max out, so storing line by line would be ideal
+    image=$1
+    x_dim=$2
+    y_dim=$3
+    x_dim_print=$4
+    y_dim_print=$5
     Arr=()
-    for (( i=0; i<$x_dim; i++ )) do
+    for (( i=0; i<$x_dim; i++ )); do
         for (( j=0; j<$y_dim; j++ )); do
-            result=`magick convert images.png -format "%[fx:int(255*p{$j,$i}.r)],%[fx:int(255*p{$j,$i}.g)],%[fx:int(255*p{$j,$i}.b)]" info:-`
+            result=`magick convert $image -format "%[fx:int(255*p{$j,$i}.r)],%[fx:int(255*p{$j,$i}.g)],%[fx:int(255*p{$j,$i}.b)]" info:-`
             Arr+=($result)
-            #echo -n "$result;" >> temp_arr_file.txt
         done
-        #echo "" >> temp_arr_file.txt
     done
 
-    #printing - chunks pixels 
-    chunk_x=$x_dim
-    chunk_y=$y_dim
-    echo ${!Arr[@]}
+    #calculate Chunk dimensions & overlap
+    # EX) 2x5 original -> 2x2; 5%2 = 1, meaning we have one "leftover" pixel.
+    # We add this extra pixel into another chunk
+
+    index=0
+    horz_offset=$x_dim%$x_dim_print
+    vert_offset=$y_dim%$y_dim_print
+    chunk_arr=()
+    
+    for (( x=0; x<$x_dim_print; x++ )); do
+        row_offset=$horz_offset
+        for (( y=0; y<$y_dim_print; y++ )); do
+            echo "yes"
+            if [[ row_offset > 0 ]]; then
+                row_offset=$((offset-1))
+                index=$(($index+1))
+            fi
+            index=$(($index+1))
+        done
+    done
+    #echo ${!Arr[@]}
 }
 
-#get_dimensions images.png
+#get_dimensions a.gif
 
-print_image 5 5
+x_dim=225
+y_dim=225
+x_dim_print=16
+y_dim_print=16
+#Copy Here
+#calculating chunk dimensions
+#offset=$x_dim%$x_dim_print
+
+#echo $(($x_dim%$x_dim_print))
+#print_image 5 5
+
+#echo `var4=1`
+#arr=(1 2 3 4 5 89)
+#get_character "${arr[@]}" 3
+
+    char_list=("@" "%" "#" "&" "=" "+" "-" ":" "," ".")
+    lenth=$((255/${#char_list[@]}))
+    incr_size=${length%%.*}
+    echo $incr_size $len $length
+    echo $incr_size $len
--- toAscii.sh
+++ toAscii.sh
@@ -5,20 +5,20 @@ check_image() {
     image_file=$1;
 
     if [[ -z $image_file ]]; then
-        echo "Please supply a valid file name"
+        echo "Please supply a valid file name" >&2
     fi
 
     if [[ ! -e $image_file ]]; then 
-        echo "file does not exist"
+        echo "file does not exist" >&2
     elif [[ ! -r $image_file || ! -f $image_file ]]; then 
-        echo "file is not readable"
+        echo "file is not readable" >&2
     fi
 
     #get list of supported file extensions (ie. png)
     if [[ -e supportedList.txt && -r supportedList.txt ]]; then
         #loading list
         file_extn_list=(`cat "supportedList.txt"`)
-        echo ${file_extn_list[@]}
+        #echo ${file_extn_list[@]}
         
         #check if file extension is part of supported list
         flag=0
@@ -26,22 +26,21 @@ check_image() {
         #echo $file_type
 
         #FIX HERE!!
-        for i in "${file_extn_list[@]}"; do
-            if [[ ${i%%//n} =~ $file_type ]]; then
+        for i in ${file_extn_list[@]}; do
+            if [[ $i =~ $file_type ]]; then
                 flag=1
             fi
-            echo ${i%%/n}
-                    echo $file_type
+            #echo ${i%%/n}
+            #echo $file_type
         done
         if [[ count -eq 0 ]]; then
-            echo "file type not supported"
+            echo "file type not supported" >&2
         fi
     else
         echo "List of supported files can not be found, so your file can not be verified. Would you like to continue? [y/n]:"
         read ans
         ans=${ans,,}
         if [[ $ans == 'n' ]]; then
-            echo "Exiting..."
             exit
         fi
     fi
@@ -49,16 +48,68 @@ check_image() {
 
 get_dimensions() {
     image=$1
-    lower=$((${#image}+1))
-    upper=$((${#image}+2))
-    echo $lower $upper
-    result=`magick identify ${image}`
+    result=`magick identify $image`
+    
     #result is in form of [file name] [file type] [dimensions]. 
-    #File type is max 4 char & dimensions max 4-4, totalling 13 chars 
+    #File type is max 4 char & dimensions max 4-4, totalling 13 chars max.
+    lower=$((${#image}+4))
+    upper=13
     result=${result:lower:upper}
-    echo $result
+
+    #removing 
+    echo ${result% *}
 }
 
+#turns string of rgb values in form "25,25,25" to array of 3 numbers
+get_rgb(){
+    rgbs=()
+    str=$@
+
+    first=${str%%,*}
+    #cut out 1st and 3rd elements
+    second=${str:$((${#first}+1)):${#str}}; second=${second%%,*}
+    third=${str##*,}
+    rgbs+=($first)
+    rgbs+=($second)
+    rgbs+=($third)
+    echo ${rgbs[@]}
+}
+
+print_image(){
+    #convert image to array
+    x_dim=$1
+    y_dim=$2
+    x_dim_print=$3
+    y_dim_print=$4
+    Arr=()
+    #Each row is x_dim_print long & Each column is y_dim_print long
+    for (( i=0; i<$x_dim; i++ )); do
+        for (( j=0; j<$y_dim; j++ )); do
+            result=`magick convert images.png -format "%[fx:int(255*p{$j,$i}.r)],%[fx:int(255*p{$j,$i}.g)],%[fx:int(255*p{$j,$i}.b)]" info:-`
+            Arr+=($result)
+            #echo -n "$result;" >> temp_arr_file.txt
+        done
+        #echo "" >> temp_arr_file.txt
+    done
+
+    #printing - chunks pixels 
+    chunk_x=$x_dim
+    chunk_y=$y_dim
+    echo ${!Arr[@]}
+}
+
+#Paramater order: [file_name] -specify_dimensions [x_length] [y_length] -background [dark/white]
+#parameters: 
+# defaults: to_x; to_y calculated 
+#Defaults 
 image=$1
-check_image $image
-get_dimensions $
+#check_image $image
+
+#-----------------------------
+#Testing stuff
+#coords=()
+coords=$(get_rgb "2,658,256")
+echo ${coords[@]}
+#get_dimensions $
+#check if dimensions are > to_x & to_y
+#if true, 
